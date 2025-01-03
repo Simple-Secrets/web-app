c09c7c4ae2f759c2b85d1e3407d74882d198547c
Vadym <kiidii@aniname.com>
2025-01-03 16:05:16 +0200

    👌 IMPROVE: integrate Tailwind CSS for styling and configure PostCSS

--- svelte.config.mjs
+++ svelte.config.mjs
@@ -1,5 +1,10 @@
+// eslint-disable-next-line import/no-unresolved
+import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
+  kit: {
+    adapter: adapter()
+  },
   preprocess: vitePreprocess(),
 }
