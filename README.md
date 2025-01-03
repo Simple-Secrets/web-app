868de632d962fc0e3d138a63263c4de17df667f6
Vadym <kiidii@aniname.com>
2025-01-03 11:48:07 +0200

    ❌ REMOVE: delete project to re-init

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/04a476d1-fe83-426b-b0c1-78a0e1dd4003/deploy-status)](https://app.netlify.com/sites/simple-secrets/deploys)
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte';
-
-const app = new App({ target: document.body });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-}
-
-export default app;
