69490629b144ebbee784944c7ec098c37d2e201b
Vadym <kiidii@aniname.com>
2025-01-03 16:20:09 +0200

    👌 IMPROVE: add DaisyUI for enhanced styling and configure custom theme in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary":   "#246BFD",
+          "secondary": "#C25FFF",
+          "accent":    "#A5F59C",
+          "neutral":   "#262A34",
+          "base-100":  "#181A20",
+          "info":      "#94F0F0",
+          "success":   "#A5F59C",
+          "warning":   "#FFDD72",
+          "error":     "#FF968E",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ]
 }
 
